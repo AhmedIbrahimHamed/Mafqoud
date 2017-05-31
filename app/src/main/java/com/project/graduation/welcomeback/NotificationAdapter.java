5fe00047214619d821fada65c3173858c0fe876e
Muhammad Attia <muhammad.mattia@gmail.com>
2017-06-01 00:50:04 +0200

    update recycle design and bind data on it

--- app/src/main/java/com/project/graduation/welcomeback/NotificationAdapter.java
+++ app/src/main/java/com/project/graduation/welcomeback/NotificationAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.firebase.client.core.Repo;
@@ -44,6 +45,11 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
     public void onBindViewHolder(NotificationViewHolder holder, int position) {
         //TODO : Add your code ya muhammad :D
 
+        holder.name.setText(mNotificationList.get(position).getName());
+        holder.age.setText(mNotificationList.get(position).getAge());
+        holder.gender.setText(mNotificationList.get(position).getGender());
+        holder.location.setText(mNotificationList.get(position).getLocation());
+        holder.photo.setImageResource(Integer.valueOf(mNotificationList.get(position).getPhoto()));
 
     }
 
@@ -60,13 +66,22 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
 
     public static class NotificationViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView notificationText;
+        public TextView name;
+        public TextView age;
+        public TextView gender;
+        public TextView location;
+        public ImageView photo ;
+
 
         public NotificationViewHolder(View itemView) {
             super(itemView);
 
-            notificationText = (TextView)itemView.findViewById(R.id.notification_text);
-
+            name = (TextView)itemView.findViewById(R.id.name);
+            age = (TextView)itemView.findViewById(R.id.age);
+            gender = (TextView)itemView.findViewById(R.id.gender);
+            location = (TextView)itemView.findViewById(R.id.location);
+            photo =(ImageView)itemView.findViewById(R.id.person_image);
+            
         }
 
     }
