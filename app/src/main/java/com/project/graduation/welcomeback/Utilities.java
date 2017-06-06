792d15ed4835bf3323911b836defde69c5922dea
AhmedIbrahimHamed <aih1175@gmail.com>
2017-06-06 04:03:53 +0200

    changed gallery name

--- app/src/main/java/com/project/graduation/welcomeback/Utilities.java
+++ app/src/main/java/com/project/graduation/welcomeback/Utilities.java
@@ -51,7 +51,7 @@ public class Utilities {
 
     private static Kairos mKairosRef;
 
-    private final static String KAIROS_GALLERY = "GalleryForMafQod";
+    private final static String KAIROS_GALLERY = "MafqoudGallery";
     public static final String KAIROS_SELECTOR = "SETPOSE";
     public static final String KAIROS_MULTIPLE_FACES = "true";
     public static final String KAIROS_MIN_HEAD_SCALE = "0.125";
